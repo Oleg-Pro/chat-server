1d702dcf5f15445e5c8c4ceea58f5d05638bb991
Oleg Proshin <opp20071980@gmail.com>
2024-10-30 15:39:46 +0300

    lesson4 - test for create and delete chat

---
+++ internal/service/chat/tests/create_test.go
@@ -0,0 +1,72 @@
+package tests
+
+import (
+	"context"
+	"testing"
+
+	"github.com/Oleg-Pro/chat-server/internal/model"
+	"github.com/Oleg-Pro/chat-server/internal/repository"
+	repoMocks "github.com/Oleg-Pro/chat-server/internal/repository/mocks"
+	"github.com/Oleg-Pro/chat-server/internal/service/chat"
+	"github.com/brianvoe/gofakeit/v6"
+	"github.com/gojuno/minimock/v3"
+	"github.com/stretchr/testify/require"
+)
+
+func TestCreate(t *testing.T) {
+	t.Parallel()
+	type userRepositoryMockFunc func(mc *minimock.Controller) repository.ChatRepository
+
+	type args struct {
+		ctx context.Context
+		req *model.ChatInfo
+	}
+
+	var (
+		ctx = context.Background()
+		mc  = minimock.NewController(t)
+
+		id           = gofakeit.Int64()
+		req = &model.ChatInfo{
+			Users: "user1,user2,user3",
+		}
+
+	)
+
+	defer t.Cleanup(mc.Finish)
+
+	tests := []struct {
+		name               string
+		args               args
+		want               int64
+		err                error
+		userRepositoryMock userRepositoryMockFunc
+	}{
+		{
+			name: "success case",
+			args: args{
+				ctx: ctx,
+				req: req,
+			},
+			want: id,
+			err:  nil,
+			userRepositoryMock: func(mc *minimock.Controller) repository.ChatRepository {
+				mock := repoMocks.NewChatRepositoryMock(mc)
+				mock.CreateMock.Expect(ctx, req).Return(id, nil)
+				return mock
+			},
+		},
+	}
+
+	for _, tt := range tests {
+		tt := tt
+		t.Run(tt.name, func(t *testing.T) {
+			t.Parallel()
+			userRepoMock := tt.userRepositoryMock(mc)
+			api := chat.New(userRepoMock)
+			resonse, err := api.Create(tt.args.ctx, tt.args.req)
+			require.Equal(t, tt.err, err)
+			require.Equal(t, tt.want, resonse)
+		})
+	}
+}
